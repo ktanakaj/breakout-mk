798d88bbd076b84e6e044b4b101292c0ff31c817
Koichi Tanaka <ktanakaj@users.noreply.github.com>
2018-08-08 15:52:35 +0900

    routerのユニットテストをとりあえず動く状態に修正

--- breakout-svr/src/test/routes/api/games.ts
+++ breakout-svr/src/test/routes/api/games.ts
@@ -1,36 +1,39 @@
-﻿// /**
-//  * @file games.jsのテスト。
-//  */
-// const assert = require('power-assert');
-// const httpMocksHelper = require('../../http-mocks-helper');
-// const models = require('../../../models');
-// const router = require('../../../routes/api/games');
-// const User = models.User;
-// const StageHeader = models.StageHeader;
-// const Stage = models.Stage;
+﻿/**
+ * @file games.tsのテスト。
+ */
+import * as assert from 'power-assert';
+import { EventEmitter } from 'events';
+import * as httpMocks from "node-mocks-http";
+import User from '../../../models/user';
+import StageHeader from '../../../models/stage-header';
+import Stage from '../../../models/stage';
+import router from '../../../routes/api/games';
 
-// describe('/api/games', () => {
-// 	before(() => {
-// 		// sqliteへのテストデータ登録
-// 		return models.sequelize.sync({ force: true })
-// 			.then(() => User.create({ id: 2, name: "test user", password: "xxxx" }))
-// 			.then(() => StageHeader.create({ id: 3, userId: 2 }))
-// 			.then(() => Stage.create({ id: 5, headerId: 3, name: "test stage", map: "[R] [G]" }));
-// 	});
+describe('/api/games', () => {
+	before(async () => {
+		// sqliteへのテストデータ登録
+		await User.create({ id: 2, name: "test user", password: "xxxx" });
+		await StageHeader.create({ id: 3, userId: 2, status: 'public' });
+		await Stage.create({ id: 5, headerId: 3, name: "test stage", map: "[R] [G]" });
+	});
 
-// 	describe('/start', () => {
-// 		it('should start game', (done) => {
-// 			const req = httpMocksHelper.createAuthorizedRequestAtUserId2({
-// 				method: 'POST',
-// 				url: '/start',
-// 				body: { stageId: 5 },
-// 			});
-// 			const res = httpMocksHelper.createResponseForJsonResult(function(playlog) {
-// 				assert.strictEqual(playlog.stageId, 5);
-// 				assert.strictEqual(playlog.userId, 2);
-// 				done();
-// 			});
-// 			router(req, res, done);
-// 		});
-// 	});
-// });
+	describe('/start', () => {
+		it('should start game', (done) => {
+			const req = httpMocks.createRequest({
+				method: 'POST',
+				url: '/start',
+				body: { stageId: 5 },
+			});
+			const res = httpMocks.createResponse({
+				eventEmitter: EventEmitter,
+			});
+			router(req, res, done);
+			res.on('end', () => {
+				const playlog = JSON.parse(res._getData());
+				assert.strictEqual(playlog.stageId, 5);
+				assert.strictEqual(playlog.userId, null);
+				done();
+			});
+		});
+	});
+});
